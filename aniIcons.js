078230414f0cd121951373f535f7f1cd273779fc
Ryan Roat <ryanroat@gmail.com>
2020-05-21 15:56:16 -0400

    hourglass drains and rotates

--- aniIcons.js
+++ aniIcons.js
@@ -10,3 +10,27 @@ function brakeChain() {
 brakeChain();
 
 setInterval(brakeChain, 2000);
+
+function runHourglass() {
+    const hourglass = document.querySelector('#hourglass');
+    hourglass.innerHTML = '&#xf251';
+
+    setTimeout(() => {
+        hourglass.innerHTML = '&#xf252';
+    }, 1000);
+    setTimeout(() => {
+        hourglass.innerHTML = '&#xf253';
+    }, 2000);
+    setTimeout(() => {
+        hourglass.classList.add('rotate');
+    }, 3000);
+    setTimeout(() => {
+        hourglass.classList.remove('rotate');
+    }, 4000);
+}
+
+runHourglass();
+
+setInterval(() => {
+    runHourglass();
+}, 4000);
